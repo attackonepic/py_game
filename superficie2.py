f0986f36d09dde29d6e2cb638a93428d006d43fd
Matias Villa <mv050266@tite.amiedu-ict.net>
2023-11-29 13:43:41 +0200

    arhivo modificado

--- superficie2.py
+++ superficie2.py
@@ -1,5 +1,4 @@
 import pygame
-
 # Iniciar juego
 pygame.init()
 # Configuracion de pantalla
@@ -9,11 +8,9 @@ height = 500
 screen = pygame.display.set_mode((width, height))
 # Titulo
 pygame.display.set_caption("Aburrido en clase")
-
 # Crear reloj
 clock = pygame.time.Clock()
 running = True
-
 # Cargar imagenes
 arsenal = pygame.image.load("arsenal.png")
 ucl = pygame.image.load("ucl.png")
@@ -22,17 +19,15 @@ saka = pygame.image.load("saka.png")
 rect_arsenal = arsenal.get_rect()
 rect_ucl = ucl.get_rect()
 rect_saka = saka.get_rect()
-
 # Inicializar posición de los rectángulos
 rect_arsenal.center = (width // 4, height // 2)
 rect_ucl.center = (3 * width // 4, height // 2)
 rect_saka.topleft = (5, 0)
-
+# Textos
 font = pygame.font.Font(None, 36)  
 text_color = (0, 0, 0)  
-
 texto = "Arsenal have won the Champions League!"
-
+gol = "1-0 to the Arsenal!"
 # Loop del juego
 while running:
     for event in pygame.event.get():
@@ -47,28 +42,38 @@ while running:
     
     # Render text
     text_surface = font.render(texto, True, text_color)
+    gol_surface = font.render(gol, True, text_color)
 
     # Get the rect of the text surface
     text_rect = text_surface.get_rect()
+    gol_rect = gol_surface.get_rect()
 
-    if keys[pygame.K_UP] and rect_arsenal.y > 0:
-        rect_arsenal.y -= 5 
-    if keys[pygame.K_DOWN] and rect_arsenal.y < height - rect_arsenal.height:
-        rect_arsenal.y += 5
-    if keys[pygame.K_LEFT] and rect_arsenal.x > 0:
-        rect_arsenal.x -= 5
-    if keys[pygame.K_RIGHT] and rect_arsenal.x < width - rect_arsenal.width:
-        rect_arsenal.x += 5
-    if keys[pygame.K_SPACE]:
-        screen.blit(saka, rect_saka)
-
+    def movement():
+        if keys[pygame.K_UP] and rect_arsenal.y > 0:
+            rect_arsenal.y -= 5 
+        if keys[pygame.K_DOWN] and rect_arsenal.y < height - rect_arsenal.height:
+            rect_arsenal.y += 5
+        if keys[pygame.K_LEFT] and rect_arsenal.x > 0:
+            rect_arsenal.x -= 5
+        if keys[pygame.K_RIGHT] and rect_arsenal.x < width - rect_arsenal.width:
+            rect_arsenal.x += 5
+        if keys[pygame.K_SPACE] and rect_saka.x < width - rect_saka.width:
+            screen.blit(saka, rect_saka)
+            rect_saka.x += 5
 
     # Ubicar los rectangulos en la pantalla
     screen.blit(arsenal, rect_arsenal)
     screen.blit(ucl, rect_ucl)
 
-    if rect_arsenal.colliderect(rect_ucl):
-        screen.blit(text_surface, text_rect)
+    def collisions():
+        if rect_arsenal.colliderect(rect_ucl):
+            screen.blit(text_surface, text_rect)
+        if rect_saka.colliderect(rect_ucl):
+            screen.blit(gol_surface, gol_rect)
+    
+    # Llamar las funciones necesarias
+    movement()
+    collisions()
 
     pygame.display.flip()
     clock.tick(60)
